d481e4e161ee7028b5c2d6f6644eaebb1217e8f9
luiscota99 <luismigcota@gmail.com>
2019-04-24 17:25:58 -0700

    administrar usuarios

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
-//import store from "@/store";
 
 export default () => {
   return axios.create({
     baseURL: `http://localhost:8081/api`
-    /*headers: {
-      Authorization: `Bearer ${store.state.token}`
-    }*/
   });
 };
--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,28 +1,7 @@
-import  axios  from "axios";
+import Api from "./Api";
 
-const url = 'http://localhost:8081/api/users'
-
-class UserService{
-    //Get User
-    static getUsers(){
-        return new Promise(async(resolve, reject) => {
-            try{
-                const res = await axios.get(url)
-                const data = res.data
-                resolve(
-                    data.map(user => ({
-                        ...user,
-                        createdAT: new Date(user.createdAT)
-                    }))
-                )
-            }
-            catch(err){
-                reject(err)
-            }
-        })
-    }
-
-    //Create User
-
-    //Delete User
-}
+export default {
+  getUsers() {
+    return Api().get("users");
+  }
+};
