879d7ae5340beb8be886b476c3b94bb3917d921e
Jocorta <jorgecordoba95@gmail.com>
2019-05-01 13:30:51 -0700

    Toma de asistencia funcional sin imagen

--- frontend/src/services/AsistenciaService.js
+++ frontend/src/services/AsistenciaService.js
@@ -3,5 +3,8 @@ import Api from "./Api";
 export default {
   getAsistencias() {
     return Api().get("asistencias");
+  },
+  addAsistencia(datos) {
+    return Api().post("asistencias", datos);
   }
 };
